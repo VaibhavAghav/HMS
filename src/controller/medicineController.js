b110ad7e4685c848cca89a6ce333f847dd8a77b0
rushikeshragde <rushikeshragde2026@gmail.com>
2025-06-11 12:08:57 +0530

    add CRUD operations for Medicine module

--- src/controller/medicineController.js
+++ src/controller/medicineController.js
@@ -1,5 +1,5 @@
 // saved medicine in model
-const medicineModel= require("../model/medicineModel")
+const medicineModel = require("../model/medicineModel")
 //1 view page for medice
 exports.addMedicinePage = (req, res) => {
     console.log("Add medicine controller page view ");
@@ -21,7 +21,72 @@ exports.savedMedicine = (req, res) => {
         console.log("Medicine saved, ID:", result.insertId);
         res.redirect("/medicine/add-medicine"); // or show success page
     });
-
 }
 
+
 // 2 save to model
+//callback funtion
+exports.getAllMedicine = (req, res) => {
+    medicineModel.getAll((err, result) => {
+        if (err) {
+            console.error("Database error:", err);
+            return res.status(500).send("Database error");
+        } else {
+            res.render("Medicine/getAllMedicine", { medicines: result });
+        }
+    });
+};
+
+
+exports.updateMedicine = (req, res) => {
+    const medicine_id = req.params.medical_id;
+    console.log(medicine_id);
+    medicineModel.getMedicine(medicine_id, (err, result) => {
+        if (err) {
+            console.log("Database error:", err);
+            return res.status(500).send("DB error");
+        } else {
+            console.log(result);
+            res.render("Medicine/updateMedicine", { medicine: result[0] });
+        }
+    });
+};
+
+exports.saveUpdatedMedicine = (req, res) => {
+    const { medicine_id, medicine_name, price_medicine } = req.body;
+    console.log(medicine_id, medicine_name, price_medicine);
+
+    medicineModel.updateMedicine(medicine_id, medicine_name, price_medicine, (err, result) => {
+        if (err) {
+            console.log("Update DB error:", err);
+            return res.status(500).send("Update failed");
+        }
+        res.redirect("/medicine/allmedicine");
+    });
+};
+
+// GET: Show delete confirmation
+exports.deleteMedicine = (req, res) => {
+    const medicine_id = req.params.medical_id;
+    medicineModel.getMedicine(medicine_id, (err, result) => {
+        if (err) {
+            console.log("Database error:", err);
+            return res.status(500).send("DB error");
+        }
+        res.render("Medicine/deleteMedicine", { medicine: result[0] });
+    });
+};
+
+// POST: Delete the medicine
+exports.confirmDelete = (req, res) => {
+    const medicine_id = req.body.medical_id;
+
+    medicineModel.deleteMedicine(medicine_id, (err, result) => {
+        if (err) {
+            console.log("Delete DB error:", err);
+            return res.status(500).send("Failed to delete medicine");
+        }
+        res.redirect("/medicine/allmedicine");
+    });
+};
+
--- src/model/medicineModel.js
+++ src/model/medicineModel.js
@@ -14,3 +14,44 @@ exports.saveMedicine = (medicine_name, price_medicine, callback) => {
         return callback(null, result);
     });
 };
+
+exports.getAll = (callback) => {
+    const getQuery = "Select *from medical"
+
+    db.query(getQuery, [], (err, result) => {
+
+        if (err) {
+            return callback(err, null)
+        }
+        return callback(null, result);
+    });
+};
+
+exports.getMedicine = (medicine_id, callback) => {
+    const selectQuery = "select * from medical where medical_id = ?"
+
+    db.query(selectQuery, [medicine_id], (err, result) => {
+        if (err) {
+            return callback(err, null)
+        }
+        return callback(null, result);
+    })
+}
+
+exports.updateMedicine = (id, name, price, callback) => {
+    const updateQuery = "UPDATE medical SET medicine_name = ?, price_medicine = ? WHERE medical_id = ?";
+    db.query(updateQuery, [name, price, id], (err, result) => {
+        if (err) return callback(err, null);
+        return callback(null, result);
+    });
+};
+
+// In medicineModel.js
+exports.deleteMedicine = (id, callback) => {
+    const deleteQuery = "DELETE FROM medical WHERE medical_id = ?";
+    db.query(deleteQuery, [id], (err, result) => {
+        if (err) return callback(err, null);
+        return callback(null, result);
+    });
+};
+
