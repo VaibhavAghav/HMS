ba87ac39ce61d71139bce743eb58ffecfcfe2bc6
VaibhavAghav <vaibhav888aghav@gmail.com>
2025-06-11 15:08:16 +0530

    added sidebar for admin panel with adim static fucnality

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,7 +3,34 @@ exports.LoginPage = (req, res) => {
   res.render("userLogin");
 };
 
-exports.AddUser = (req, res) => {
-    console.log("Inside controller post");
 
-}
+exports.UserLogin = (req, res) => {
+  console.log("Inside controller ");
+  console.log(req.body);
+
+  const { username, password, role } = req.body;
+
+  if (!username || !password || !role) {
+    return res.status(400).send("Username, password, and role are required");
+  }
+
+  if (username === "admin" && password === "admin" && role === "admin") {
+    console.log("Login successful admin");
+    req.session.admin = {
+      user_id: 1,
+      username: "admin",
+      role: "admin",
+    };
+    return res.redirect("/admin");
+  } else if (role === "doctor") {
+    console.log("Login successful doctor");
+    req.session.user = { username, role };
+    return res.redirect("/doctor");
+  } else if (role === "receptionist") {
+    console.log("Login successful receptionist");
+    req.session.user = { username, role };
+    return res.redirect("/receptionist");
+  } else {
+    return res.status(400).send("Invalid credentials or role");
+  }
+};
